be54792b19a6181a328c0c8b8e5e80a302255d73
itleppa <itleppa@ITDEWHMM76490.fitads01.fxdir.net>
2017-09-15 10:51:23 +0200

    tests

--- SortingAlgorithms/test/de/dhbw/horb/programmieren/projekt/test/AllTest.java
+++ SortingAlgorithms/test/de/dhbw/horb/programmieren/projekt/test/AllTest.java
@@ -4,10 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import de.dhbw.horb.programmieren.projekt.algorithms.test.SortAlgorithmTest;
+import de.dhbw.horb.programmieren.projekt.algorithms.test.QuicksortTest;
+import de.dhbw.horb.programmieren.projekt.algorithms.test.MergesortTest;
+import de.dhwb.horb.programmieren.projekt.view.ViewTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({SortAlgorithmTest.class })
+@SuiteClasses({ViewTest.class, QuicksortTest.class, MergesortTest.class})
 public class AllTest {
 
 }
---
+++ SortingAlgorithms/test/de/dhwb/horb/programmieren/projekt/view/ViewTest.java
@@ -0,0 +1,73 @@
+package de.dhwb.horb.programmieren.projekt.view;
+
+import java.util.concurrent.TimeoutException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+
+import de.dhbw.horb.programmieren.projekt.SortingApp;
+
+@RunWith(JUnit4.class)
+public class ViewTest {
+	
+	public SortingApp start;
+	private FxRobot robot;
+	private String inputString = "15,18,8,26,3,17,9,4,6,11,13,1,20";
+
+	@Before
+	public void setUp() throws Exception {
+		FxToolkit.registerPrimaryStage();
+		start = (SortingApp) FxToolkit.setupApplication(SortingApp.class);
+		robot = new FxRobot();
+		robot.sleep(1000);
+	}	
+	
+	@Test
+	public void test() {
+		robot.clickOn("#rbtnFile");
+		robot.clickOn("#btnBrowse");
+		robot.sleep(1000);
+		robot.closeCurrentWindow();
+		robot.clickOn("#rbtnManual");
+		robot.clickOn("#tfManual");
+		robot.eraseText(20);
+		robot.write(inputString);
+		robot.sleep(1000);
+		robot.clickOn("#tfThreads");
+		robot.eraseText(2);
+		robot.write("4");
+		robot.clickOn("#moreOptionsPane");
+		robot.sleep(1000);
+		robot.clickOn("#chkConsoleOutput");
+		robot.clickOn("#tfDelay");
+		robot.eraseText(2);
+		robot.write("0");
+		robot.clickOn("#btnStart");
+		robot.sleep(3000);
+		robot.clickOn("#rbtnRandom");
+		robot.clickOn("#tfAmount");
+		robot.eraseText(10);
+		robot.write("2000");
+		robot.clickOn("#rbtnMergeSort");
+		robot.clickOn("#tfDelay");
+		robot.eraseText(3);
+		robot.write("20");
+		robot.clickOn("#chkAnimation");
+		robot.clickOn("#btnStart");
+		robot.sleep(10000);
+	}
+	
+	@After
+	public void clean() {
+		try {
+			FxToolkit.cleanupStages();
+		} catch (TimeoutException e) {
+			e.printStackTrace();
+		}
+	}
+}
